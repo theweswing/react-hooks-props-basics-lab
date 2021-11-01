ec1c6c564b8028369cc6f7f9ec9eb60bb8cc8b9c
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-01 10:51:04 -0400

    display and features match the example and all links work but 2 tests still fail

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,37 @@
 import React from "react";
 
-function About() {
+// function Links() {
+//   return (
+//     <h3>Links</h3>
+//     <a href={props.github}></a>
+//     <a href={props.linkedin}
+//   )
+// }
+
+function DisplayBio(props){
+  if (props.bio) {
+    return (<p>{props.bio}</p>)
+  }
+}
+
+function Links(props){
+  return (
+    <div>
+    <h3>Links</h3>
+    <a href={props.github}>{props.github}</a>
+    <a href={props.linkedin}>{props.github}</a>
+    </div>
+  )
+}
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+            {DisplayBio(props)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {Links(props)}
     </div>
   );
 }
